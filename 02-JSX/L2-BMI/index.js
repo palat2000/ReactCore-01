7ff788820d508847ff4d2429b69ae09dc9dc2208
palat2000 <palat.tungsantitham@gmail.com>
2023-08-22 23:16:44 +0700

    update

--- 02-JSX/L2-BMI/index.js
+++ 02-JSX/L2-BMI/index.js
@@ -1,5 +1,10 @@
 // #1 React : JSX
-
+let height = 1.75;
+let weight = 75;
+const calcBMI = (height, weight) => weight / (height**2);
+const render = <h3>BMI : {calcBMI(height,weight)}</h3>
 // #2 ReactDOM : createRoot
-
+const domRoot = document.getElementById("root");
+const root = ReactDOM.createRoot(domRoot);
 // #3 ReactDOM : TakeOver for Render Task
+root.render(render);
