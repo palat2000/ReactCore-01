3ca24949d044ee24c4584d54e68a16686a0e1d52
palat2000 <palat.tungsantitham@gmail.com>
2023-08-23 13:29:34 +0700

    update

--- 03-ReactComponent/_demo1_UserProfile/index.js
+++ 03-ReactComponent/_demo1_UserProfile/index.js
@@ -1,6 +1,39 @@
+const userProfile = (
+  <div className="profile_card">
+    <h1>FullName : John Doe</h1>
+    <h2>Age : 30</h2>
+  </div>
+);
+// const appComponent = (
+//   <>
+//     <UserProfile />
+//     <UserProfile />
+//     <UserProfile />
+//   </>
+// );
+
+//Component
+function UserProfile() {
+  return (
+    <div className="profile_card">
+      <h1>FullName : John Doe</h1>
+      <h2>Age : 30</h2>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="app">
+      <UserProfile />
+      <UserProfile />
+      <UserProfile />
+    </div>
+  );
+}
 // #2 ReactDOM : createRoot
-const domNode = document.getElementById('root');
+const domNode = document.getElementById("root");
 const root = ReactDOM.createRoot(domNode);
 
 // #3 ReactDOM : TakeOver for Render Task
-root.render(<h1>Hello React</h1>);
+root.render(<App />);
